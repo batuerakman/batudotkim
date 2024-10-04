9f8927643e61b10e6e16d445d5fe4ae6ad2a2925
Batu <62992069+batuerakman@users.noreply.github.com>
2024-10-04 14:22:57 +0300

    style: add button select widget

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,8 @@
 import 'package:batu/theme/texttheme.dart';
-import 'package:batu/widgets/dashboard.dart';
 import 'package:flutter/material.dart';
+import '../widgets/dashboard.dart';
 import '../widgets/background_gradient.dart';
+import '../widgets/user_row.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -13,10 +14,13 @@ class HomePage extends StatelessWidget {
         children: [
           const BackgroundGradient(),
           Padding(
-            padding: const EdgeInsets.all(64),
+            padding: const EdgeInsets.all(48),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
+                const UserRow(),
+                const SizedBox(height: 64),
                 Center(
                   child: SizedBox(
                     width: 1350,
@@ -39,7 +43,20 @@ class HomePage extends StatelessWidget {
                 const DashboardWidget(),
               ],
             ),
-          )
+          ),
+          Positioned(
+              bottom: 48,
+              left: 128,
+              child: Row(
+                children: [
+                  const Image(height: 32, image: AssetImage('assets/icons/buttonA.png')),
+                  const SizedBox(width: 12),
+                  Text(
+                    'Select',
+                    style: buttonPressLabel,
+                  )
+                ],
+              ))
         ],
       ),
     );
